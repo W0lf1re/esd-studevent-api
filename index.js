d63132d754460cc653bbe32b18bf741702f39a22
Yvann <yvann.nadal@mail-esd.com>
2022-06-30 10:25:10 +0200

    help me

--- index.js
+++ index.js
@@ -4,25 +4,69 @@ const port = 8000
 
 const morgan = require('morgan')
 
+app.use(express.json())
 app.use(morgan('dev'))
 
-app.get('/users', (req, res) => {
-  res.send('Get all users')
+const events = [
+  {
+    id: 1,
+    name: 'Event 1',
+    date: '2020-01-01',
+    time: '12:00',
+    description: "This is the description of event 1",
+    author: "John Doe",
+    image: "https://placehold.it/300x300",
+  },
+  {
+    id: 2,
+    name: 'Event 2',
+    date: '2020-01-01',
+    time: '12:00',
+    description: "This is the description of event 2",
+    author: "John Doe",
+    image: "https://placehold.it/300x300",
+  },
+]
+
+app.get('/events', (req, res) => {
+  res.send(events)
 })
-app.get('/users/:id', (req, res) => {
-  console.log("get one user : ", req.params.id)
-  res.send('Get one user')
+app.get('/events/:id', (req, res) => {
+  const { id } = req.params
+  const event = events.find(event => event.id == id)
+  if (!event) {
+    res.status(404).send({ message: `Event with id ${id} not found` })
+  }
+  res.send(event)
 })
-app.delete('/users/:id', (req, res) => {
-  console.log("delete user : ", req.params.id)
-  res.send('Delete one user')
+app.delete('/events/:id', (req, res) => {
+  const { id } = req.params
+  const event = events.find(event => event.id == id)
+  if (!event) {
+    res.status(404).send({ message: `Event with id ${id} not found` })
+  }
+  events.splice(events.indexOf(event), 1)
+  res.send(event)
 })
-app.put('/users/:id', (req, res) => {
-  console.log("put user : ", req.params.id)
-  res.send('Update one user')
+app.put('/events/:id', (req, res) => {
+  const { id } = req.params
+  let event = events.find(event => event.id == id)
+  if (!event) {
+    res.status(404).send({ message: `Event with id ${id} not found` })
+  }
+  events[events.indexOf(event)] = {
+    ...event,
+    ...req.body
+  }
+  res.send(event)
 })
-app.post('/users', (req, res) => {
-  res.send('Add new user')
+app.post('/events', (req, res) => {
+  const event = {
+    id: events.length + 1,
+    ...req.body
+  }
+  events.push(event)
+  res.send(event)
 })
 
 app.listen(port, () => {
